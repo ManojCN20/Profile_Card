1a9e036b272501b4ece8625da2c6758ac0c5d99f
Manoj C N <manojcn0302@gmail.com>
2023-12-12 11:21:24 +0530

    Completed

--- src/index.js
+++ src/index.js
@@ -37,14 +37,16 @@ const skills = [
 
 function App() {
   return (
-    <div className="card">
-      <Avatar />
-      <div className="data">
-        <Intro />
-        {/* Should contain one Skill component
+    <div className="container">
+      <div className="card">
+        <Avatar />
+        <div className="data">
+          <Intro />
+          {/* Should contain one Skill component
         for each web dev skill that you have,
         customized with props */}
-        <SkillList />
+          <SkillList />
+        </div>
       </div>
     </div>
   );
